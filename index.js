a7037e13979b6a2743e5c2cddcc40d58a4740d1e
gitjs321 <githubpractice181@gmail.com>
2026-01-22 23:48:00 +0530

    commit-01

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+
+import mongoose from 'mongoose'
+import userRouter from './routers/userRouter.js';
+
+
+
+let mongoURI = "mongodb+srv://admin:1234@cluster0.jg4p067.mongodb.net/MYDB?appName=Cluster0";
+
+mongoose.connect(mongoURI)
+.then(()=>{
+    console.log("MongoDB connected")
+})
+.catch((err)=>{
+    console.log("MongoDB connection error:", err)
+})
+
+let app=express();
+
+app.use(express.json());
+
+app.use("/Users",userRouter)
+
+
+app.listen(3000, ()=>{
+    console.log("server started...")
+});
---
+++ models/user.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type:String,
+        required: true,
+        unique: true
+    },
+    firstName:{
+        type:String,
+        required: true
+    },
+    lastName: {
+        type:String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["admin","customer"],
+        default: "customer"
+    },
+    isBlocked: {
+        type:Boolean,
+        isBlocked: false
+    },
+    isEmailVerified: {
+        type :Boolean,
+        default: false,
+        required:true
+    },
+
+    image: {
+        type:String,
+        default: "/images/image.img",
+        required :true
+    }
+});
+const User= mongoose.model('User', userSchema);
+export default User;
+   
